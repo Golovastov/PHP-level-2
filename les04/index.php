9d0481f88ae8e2f02c6b1ea0486bac45d7fc6416
Konstantin <kotjane@ya.ru>
2018-07-04 19:29:36 +0300

    work

--- les04/index.php
+++ les04/index.php
@@ -1,2 +1,17 @@
 <?php
+require_once 'vendor/autoload.php';
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'brend';
 
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $password);
+    $sql = "SELECT * FROM `les04`";
+    $result = $pdo->query($sql);
+    $loader = new Twig_Loader_Filesystem('public');
+    $twig = new Twig_Environment($loader);
+    $catalog = $twig->load('');
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
